c440327071ba110320a33e4a8a3747f0d61cbb6e
DhruvBharwada <145761379+DhruvBharwada@users.noreply.github.com>
2026-06-12 10:38:11 +0530

    Add: boats-to-save-people - submission-4

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-4.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int numRescueBoats(int[] nums, int limit) {
+        int[] count = new int[limit + 1];
+        for (int num : nums) {
+            count[num]++;
+        }
+        int index = 0;
+        // for (int i = 0; i <= limit; i++) {
+        //     while (count[i] > 0) {
+        //         nums[index] = i;
+        //         index++;
+        //         count[i]--;
+        //     }
+        // }
+        int i = 0;
+        int j = 0;
+        while(i < count.length){
+            if(count[i]>0){
+                nums[j] = i;
+                j++;
+                count[i]--;
+            }else{
+                i++;
+            }
+        }
+
+
+        int left = 0;
+        int right = nums.length-1;
+        int cnt = 0;
+        while(left<=right){
+            if(nums[right]==limit){
+                right--;
+            }else if(nums[left]+nums[right]== limit){
+                right--;
+                left++;
+            }else if(nums[left]+nums[right]>limit){
+                right--;
+            }else if(nums[left]+nums[right]<limit){
+                right--;
+                left++;
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+}
